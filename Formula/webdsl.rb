fe550a679cee0fc3e74a514dc749e75ffe5f3424
William Cotton <williamc@eurekasoft.com>
2025-01-13 08:10:50 -0600

    use llvm clang

--- Formula/webdsl.rb
+++ Formula/webdsl.rb
@@ -15,8 +15,18 @@ class Webdsl < Formula
   depends_on "curl"
 
   def install
-    ENV.llvm_clang
-    system "make"
+    llvm = Formula["llvm"]
+    
+    # Set environment variables for compilation
+    ENV["CC"] = llvm.opt_bin/"clang"
+    ENV["LDFLAGS"] = "-L#{llvm.opt_lib}"
+    ENV["CPPFLAGS"] = "-I#{llvm.opt_include}"
+    
+    # Ensure pkg-config can find the Homebrew-installed libraries
+    ENV.prepend_path "PKG_CONFIG_PATH", Formula["postgresql@14"].opt_lib/"pkgconfig"
+    
+    # Override the make command to use the correct compiler
+    system "make", "build/webdsl", "CC=#{llvm.opt_bin}/clang"
     bin.install "build/webdsl"
   end
 
